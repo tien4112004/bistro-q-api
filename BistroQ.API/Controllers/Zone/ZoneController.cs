95bd86713de64a337b073442b79c583c1b327e04
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2024-10-30 09:47:26 +0700

    Implement Zone controller (user)

---
+++ BistroQ.API/Controllers/Zone/ZoneController.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using BistroQ.Core.Dtos;
+using BistroQ.Core.Dtos.Zones;
+using BistroQ.Core.Enums;
+using BistroQ.Core.Exceptions;
+using BistroQ.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BistroQ.API.Controllers;
+
+[ApiController]
+[Route("api/Admin/[controller]")]
+[Authorize]
+[Tags("Admin Zone")]
+public class AdminZoneController : ControllerBase
+{
+    private readonly IZoneService _zoneService;
+    
+    public AdminZoneController(IZoneService zoneService)
+    {
+        _zoneService = zoneService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetZones([FromQuery] ZoneCollectionQueryParams queryParams)
+    {
+        var (zones, count) = await _zoneService.GetAllAsync(queryParams);
+
+        return Ok(new PaginationResponseDto<IEnumerable<ZoneDto>>(zones, count, queryParams.Page, queryParams.Size));
+    }
+
+    [HttpGet]
+    [Route("{zoneId:int}")]
+    public async Task<IActionResult> GetZone([FromRoute] int zoneId)
+    {
+        var zone = await _zoneService.GetByIdAsync(zoneId);
+        if (zone == null)
+        {
+            throw new ResourceNotFoundException("Zone not found");
+        }
+
+        return Ok(new ResponseDto<ZoneDto>(zone));
+    }
+}
--- BistroQ.Core/Mappings/MappingProfile.cs
+++ BistroQ.Core/Mappings/MappingProfile.cs
@@ -18,15 +18,10 @@ public class MappingProfile : Profile
             .ConstructUsing((src, context) => new Product());
         
         CreateMap<Zone, ZoneDto>().ReverseMap();
-
         CreateMap<CreateZoneRequestDto, Zone>().ConstructUsing((src, context) => new Zone());
-
         CreateMap<UpdateZoneRequestDto, Zone>().ConstructUsing((src, context) => new Zone());
-
-        // CreateMap<CreateZoneRequestDto, Zone>()
-        //     .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
-        //
-        // CreateMap<UpdateZoneRequestDto, Zone>()
-        //     .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+        
+        
+        
     }
 }
