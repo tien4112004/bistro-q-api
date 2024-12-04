ea7160f7a8caf3524436c7520b0b9a6b767466fe
Thanh Tien Phan <tien4112004@gmail.com>
2024-12-04 08:53:15 +0700

    Change `ClientOrder` route to `Client/Order`

--- BistroQ.API/Controllers/Order/ClientOrderController.cs
+++ BistroQ.API/Controllers/Order/ClientOrderController.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using BistroQ.Core.Dtos;
 using BistroQ.Core.Dtos.Orders;
-using BistroQ.Core.Entities;
 using BistroQ.Core.Enums;
 using BistroQ.Core.Exceptions;
 using BistroQ.Core.Interfaces.Services;
@@ -13,84 +11,84 @@ using Microsoft.AspNetCore.Mvc;
 namespace BistroQ.API.Controllers.Order;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/Client/Order")]
 [Authorize(Roles = BistroRoles.Client)]
 [Tags("Client Order")]
 public class ClientOrderController : ControllerBase
 {
-    private readonly IOrderService _orderService;
-    private readonly UserManager<AppUser> _userManager;
+	private readonly IOrderService _orderService;
+	private readonly UserManager<AppUser> _userManager;
 
-    public ClientOrderController(IOrderService orderService, UserManager<AppUser> userManager)
-    {
-        _orderService = orderService;
-        _userManager = userManager;
-    }
-    
-    [HttpPost]
-    public async Task<IActionResult> CreateOrder(int peopleCount)
-    {
-        var tableId = await GetTableId();
-        
-        var order = await _orderService.CreateOrder(tableId, peopleCount);
-        return Ok(new ResponseDto<OrderDto>(order));
-    }
-    
-    [HttpGet]
-    public async Task<IActionResult> GetOrder()
-    {
-        var tableId = await GetTableId();
-        
-        var order = await _orderService.GetOrder(tableId);
-        return Ok(new ResponseDto<DetailOrderDto>(order));
-    }
-    
-    [HttpDelete]
-    public async Task<IActionResult> DeleteOrder()
-    {
-        var tableId = await GetTableId();
-        
-         await _orderService.DeleteOrder(tableId);
-        return Ok(new ResponseDto<string>("Order deleted"));
-    }
-    
-    [HttpPatch("PeopleCount")]
-    public async Task<IActionResult> UpdatePeopleCount([FromQuery] int peopleCount)
-    {
-        var tableId = await GetTableId();
-        
-        var order = await _orderService.UpdatePeopleCount(tableId, peopleCount);
-        return Ok(new ResponseDto<OrderDto>(order));
-    }
-        
-    [HttpPost("Items")]
-    public async Task<IActionResult> AddProductsToOrder([FromBody] IEnumerable<CreateOrderItemRequestDto> orderItems)
-    {
-        var tableId = await GetTableId();
-    
-        var addedItems = await _orderService.AddProductsToOrder(tableId, orderItems);
-    
-        return Ok(new ResponseDto<IEnumerable<OrderItemDto>>(addedItems));
-    }
-    
-    [HttpDelete]
-    [Route("Items")]
-    public async Task<IActionResult> RemoveProductsFromOrder([FromBody] IEnumerable<RemoveOrderItemRequestDto> orderItems)
-    {
-        var tableId = await GetTableId();
-        
-        var cancelledItems = await _orderService.CancelOrderItems(tableId, orderItems);
-        return Ok(new ResponseDto<IEnumerable<OrderItemDto>>(cancelledItems));
-    }
-    
-    private async Task<int> GetTableId()
-    {
-        var user = await _userManager.GetUserAsync(User);
-        if (user?.TableId == null)
-        {
-            throw new UnauthorizedException("User not found");
-        }
+	public ClientOrderController(IOrderService orderService, UserManager<AppUser> userManager)
+	{
+		_orderService = orderService;
+		_userManager = userManager;
+	}
 
-        return user.TableId.Value;
-    }
+	[HttpPost]
+	public async Task<IActionResult> CreateOrder(int peopleCount)
+	{
+		var tableId = await GetTableId();
+
+		var order = await _orderService.CreateOrder(tableId, peopleCount);
+		return Ok(new ResponseDto<OrderDto>(order));
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetOrder()
+	{
+		var tableId = await GetTableId();
+
+		var order = await _orderService.GetOrder(tableId);
+		return Ok(new ResponseDto<DetailOrderDto>(order));
+	}
+
+	[HttpDelete]
+	public async Task<IActionResult> DeleteOrder()
+	{
+		var tableId = await GetTableId();
+
+		await _orderService.DeleteOrder(tableId);
+		return Ok(new ResponseDto<string>("Order deleted"));
+	}
+
+	[HttpPatch("PeopleCount")]
+	public async Task<IActionResult> UpdatePeopleCount([FromQuery] int peopleCount)
+	{
+		var tableId = await GetTableId();
+
+		var order = await _orderService.UpdatePeopleCount(tableId, peopleCount);
+		return Ok(new ResponseDto<OrderDto>(order));
+	}
+
+	[HttpPost("Items")]
+	public async Task<IActionResult> AddProductsToOrder([FromBody] IEnumerable<CreateOrderItemRequestDto> orderItems)
+	{
+		var tableId = await GetTableId();
+
+		var addedItems = await _orderService.AddProductsToOrder(tableId, orderItems);
+
+		return Ok(new ResponseDto<IEnumerable<OrderItemDto>>(addedItems));
+	}
+
+	[HttpDelete]
+	[Route("Items")]
+	public async Task<IActionResult> RemoveProductsFromOrder([FromBody] IEnumerable<RemoveOrderItemRequestDto> orderItems)
+	{
+		var tableId = await GetTableId();
+
+		var cancelledItems = await _orderService.CancelOrderItems(tableId, orderItems);
+		return Ok(new ResponseDto<IEnumerable<OrderItemDto>>(cancelledItems));
+	}
+
+	private async Task<int> GetTableId()
+	{
+		var user = await _userManager.GetUserAsync(User);
+		if (user?.TableId == null)
+		{
+			throw new UnauthorizedException("User not found");
+		}
+
+		return user.TableId.Value;
+	}
 }
