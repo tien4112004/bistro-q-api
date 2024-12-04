5ed618f303b162fe4873f4a2f4cea097c5c03db9
Thanh Tien Phan <tien4112004@gmail.com>
2024-12-04 08:23:38 +0700

    Add price to `CreateOrderItemRequestDto`

--- BistroQ.Core/Dtos/Orders/CreateOrderItemRequestDto.cs
+++ BistroQ.Core/Dtos/Orders/CreateOrderItemRequestDto.cs
@@ -2,6 +2,7 @@
 
 public class CreateOrderItemRequestDto
 {
-    public int ProductId { get; set; }
-    public int Quantity { get; set; }
+	public int ProductId { get; set; }
+	public int Quantity { get; set; }
+	public decimal PriceAtPurchase { get; set; }
 }
--- BistroQ.Services/Services/OrderService.cs
+++ BistroQ.Services/Services/OrderService.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices.Marshalling;
 using AutoMapper;
 using BistroQ.Core.Dtos.Orders;
 using BistroQ.Core.Entities;
@@ -11,152 +10,155 @@ namespace BistroQ.Services.Services;
 
 public class OrderService : IOrderService
 {
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly IMapper _mapper;
-    
-    public OrderService(IUnitOfWork unitOfWork, IMapper mapper)
-    {
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
-    }
+	private readonly IUnitOfWork _unitOfWork;
+	private readonly IMapper _mapper;
 
-    public async Task<OrderDto> CreateOrder(int tableId, int peopleCount)
-    {
-        var order = new Order
-        {
-            OrderId = Guid.NewGuid().ToString(),
-            TableId = tableId,
-            TotalAmount = 0,
-            StartTime = DateTime.Now,
-            EndTime = null,
-            PeopleCount = peopleCount,
-        };
-        
-        var table = await _unitOfWork.TableRepository.GetByIdAsync(tableId);
-        if (table == null)
-        {
-            throw new ResourceNotFoundException("Table not found");
-        }
+	public OrderService(IUnitOfWork unitOfWork, IMapper mapper)
+	{
+		_unitOfWork = unitOfWork;
+		_mapper = mapper;
+	}
 
-        var existingOrder = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
-        if (existingOrder != null)
-        {
-            throw new ConflictException("Order already exists");
-        }
-        
-        var createdOrder = await _unitOfWork.OrderRepository.AddAsync(order);
-        await _unitOfWork.SaveChangesAsync();
-        
-        return _mapper.Map<OrderDto>(createdOrder);
-    }
+	public async Task<OrderDto> CreateOrder(int tableId, int peopleCount)
+	{
+		var order = new Order
+		{
+			OrderId = Guid.NewGuid().ToString(),
+			TableId = tableId,
+			TotalAmount = 0,
+			StartTime = DateTime.Now,
+			EndTime = null,
+			PeopleCount = peopleCount,
+		};
 
-    public async Task<DetailOrderDto> GetOrder(int tableId)
-    {
-        var order =await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
-        if (order == null)
-        {
-            throw new ResourceNotFoundException("Order not found");
-        }
-        
-        order.OrderItems = _mapper.Map<List<OrderItem>>(order.OrderItems);
+		var table = await _unitOfWork.TableRepository.GetByIdAsync(tableId);
+		if (table == null)
+		{
+			throw new ResourceNotFoundException("Table not found");
+		}
 
-        return _mapper.Map<DetailOrderDto>(order);
-    }
-    
-    public async Task DeleteOrder(int tableId)
-    {
-        var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
-        if (order == null)
-        {
-            throw new ResourceNotFoundException("Order not found");
-        }
+		var existingOrder = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
+		if (existingOrder != null)
+		{
+			throw new ConflictException("Order already exists");
+		}
 
-        await _unitOfWork.OrderRepository.DeleteAsync(order);
-        await _unitOfWork.SaveChangesAsync();
-    }
-    
-    public async Task<IEnumerable<OrderWithTableDto>> GetAllCurrentOrders()
-    {
-        var orders = await _unitOfWork.OrderRepository.GetAllCurrentOrdersAsync();
-        return _mapper.Map<IEnumerable<OrderWithTableDto>>(orders);
-    }
+		var createdOrder = await _unitOfWork.OrderRepository.AddAsync(order);
+		await _unitOfWork.SaveChangesAsync();
 
-    public async Task<IEnumerable<OrderItemDto>> AddProductsToOrder(
-        int tableId, 
-        [FromBody] IEnumerable<CreateOrderItemRequestDto> orderItems)
-    {
-        var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
-        if (order == null)
-        {
-            throw new ResourceNotFoundException("Order not found");
-        }
-        
-        List<OrderItem> addedItems = new List<OrderItem>();
-        
-        foreach (var item in orderItems)
-        {
-            var product = await _unitOfWork.ProductRepository.GetByIdAsync(item.ProductId);
-            if (product == null)
-            {
-                throw new ResourceNotFoundException("Product not found");
-            }
-            
-            var orderItem = new OrderItem
-            {
-                OrderId = order.OrderId,
-                ProductId = item.ProductId,
-                Quantity = item.Quantity,
-                PriceAtPurchase = product.DiscountPrice ?? product.Price,
-            };
-            
-            var addedItem = await _unitOfWork.OrderItemRepository.AddAsync(orderItem);
-            addedItems.Add(addedItem);
-        }
-        
-        await _unitOfWork.SaveChangesAsync();
-        var addedItemsDto = _mapper.Map<IEnumerable<OrderItemDto>>(addedItems);
+		return _mapper.Map<OrderDto>(createdOrder);
+	}
 
-        return addedItemsDto;
-    }
-    
-    public async Task<IEnumerable<OrderItemDto>> CancelOrderItems(int tableId, [FromBody] IEnumerable<RemoveOrderItemRequestDto> orderItems)
-    {
-        var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
-        if (order == null)
-        {
-            throw new ResourceNotFoundException("Order not found");
-        }
-        
-        List<OrderItem> removedItems = new List<OrderItem>();
-        foreach (var item in orderItems)
-        {
-            var orderItem = order.OrderItems.FirstOrDefault(oi => oi.OrderItemId == item.OrderItemId);
-            if (orderItem == null)
-            {
-                throw new ResourceNotFoundException("OrderItem not found");
-            }
-            
-            await _unitOfWork.OrderItemRepository.DeleteAsync(orderItem);
-            removedItems.Add(orderItem);
-        }
-        await _unitOfWork.SaveChangesAsync();
-    
-        return _mapper.Map<IEnumerable<OrderItemDto>>(removedItems);
-    }
+	public async Task<DetailOrderDto> GetOrder(int tableId)
+	{
+		var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
+		if (order == null)
+		{
+			throw new ResourceNotFoundException("Order not found");
+		}
 
-    public async Task<OrderDto> UpdatePeopleCount(int tableId, int peopleCount)
-    {
-        var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
-        if (order == null)
-        {
-            throw new ResourceNotFoundException("Order not found");
-        }
-        
-        var updatedOrder = order;
-        updatedOrder.PeopleCount = peopleCount;
-        await _unitOfWork.OrderRepository.UpdateAsync(order, updatedOrder);
-        await _unitOfWork.SaveChangesAsync();
+		order.OrderItems = _mapper.Map<List<OrderItem>>(order.OrderItems);
 
-        return _mapper.Map<OrderDto>(updatedOrder);
-    }
+		return _mapper.Map<DetailOrderDto>(order);
+	}
+
+	public async Task DeleteOrder(int tableId)
+	{
+		var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
+		if (order == null)
+		{
+			throw new ResourceNotFoundException("Order not found");
+		}
+
+		await _unitOfWork.OrderRepository.DeleteAsync(order);
+		await _unitOfWork.SaveChangesAsync();
+	}
+
+	public async Task<IEnumerable<OrderWithTableDto>> GetAllCurrentOrders()
+	{
+		var orders = await _unitOfWork.OrderRepository.GetAllCurrentOrdersAsync();
+		return _mapper.Map<IEnumerable<OrderWithTableDto>>(orders);
+	}
+
+	public async Task<IEnumerable<OrderItemDto>> AddProductsToOrder(
+		int tableId,
+		[FromBody] IEnumerable<CreateOrderItemRequestDto> orderItems)
+	{
+		var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
+		if (order == null)
+		{
+			throw new ResourceNotFoundException("Order not found");
+		}
+		var updatedOrder = order;
+
+		List<OrderItem> addedItems = new List<OrderItem>();
+
+		foreach (var item in orderItems)
+		{
+			var product = await _unitOfWork.ProductRepository.GetByIdAsync(item.ProductId);
+			if (product == null)
+			{
+				throw new ResourceNotFoundException("Product not found");
+			}
+
+			var orderItem = new OrderItem
+			{
+				OrderId = order.OrderId,
+				ProductId = item.ProductId,
+				Quantity = item.Quantity,
+				PriceAtPurchase = product.Price, // TODO: OR DiscountPrice
+			};
+
+			var addedItem = await _unitOfWork.OrderItemRepository.AddAsync(orderItem);
+			addedItems.Add(addedItem);
+			order.TotalAmount += (item.PriceAtPurchase * item.Quantity);
+		}
+
+		await _unitOfWork.OrderRepository.UpdateAsync(order, updatedOrder);
+		await _unitOfWork.SaveChangesAsync();
+		var addedItemsDto = _mapper.Map<IEnumerable<OrderItemDto>>(addedItems);
+
+		return addedItemsDto;
+	}
+
+	public async Task<IEnumerable<OrderItemDto>> CancelOrderItems(int tableId, [FromBody] IEnumerable<RemoveOrderItemRequestDto> orderItems)
+	{
+		var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
+		if (order == null)
+		{
+			throw new ResourceNotFoundException("Order not found");
+		}
+
+		List<OrderItem> removedItems = new List<OrderItem>();
+		foreach (var item in orderItems)
+		{
+			var orderItem = order.OrderItems.FirstOrDefault(oi => oi.OrderItemId == item.OrderItemId);
+			if (orderItem == null)
+			{
+				throw new ResourceNotFoundException("OrderItem not found");
+			}
+
+			await _unitOfWork.OrderItemRepository.DeleteAsync(orderItem);
+			removedItems.Add(orderItem);
+		}
+		await _unitOfWork.SaveChangesAsync();
+
+		return _mapper.Map<IEnumerable<OrderItemDto>>(removedItems);
+	}
+
+	public async Task<OrderDto> UpdatePeopleCount(int tableId, int peopleCount)
+	{
+		var order = await _unitOfWork.OrderRepository.GetByTableIdAsync(tableId);
+		if (order == null)
+		{
+			throw new ResourceNotFoundException("Order not found");
+		}
+
+		var updatedOrder = order;
+		updatedOrder.PeopleCount = peopleCount;
+		await _unitOfWork.OrderRepository.UpdateAsync(order, updatedOrder);
+		await _unitOfWork.SaveChangesAsync();
+
+		return _mapper.Map<OrderDto>(updatedOrder);
+	}
 }
