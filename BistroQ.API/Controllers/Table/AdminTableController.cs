1eaa5e8e50a26839df506cd5e2f8dd5731fbe829
Thanh Tien Phan <tien4112004@gmail.com>
2024-12-04 10:49:22 +0700

    Add tests for CRUD routes `AdminZone`, `AdminTable`

--- BistroQ.API/Controllers/Table/AdminTableController.cs
+++ BistroQ.API/Controllers/Table/AdminTableController.cs
@@ -1,8 +1,6 @@
-﻿using System.Text.Json;
-using BistroQ.Core.Dtos;
+﻿using BistroQ.Core.Dtos;
 using BistroQ.Core.Dtos.Tables;
 using BistroQ.Core.Enums;
-using BistroQ.Core.Exceptions;
 using BistroQ.Core.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,48 +13,58 @@ namespace BistroQ.API.Controllers;
 [Tags("Admin Table")]
 public class AdminTableController : ControllerBase
 {
-    private readonly ITableService _tableService;
-    
-    public AdminTableController(ITableService tableService)
-    {
-        _tableService = tableService;
-    }
-    
-    [HttpGet]
-    public async Task<IActionResult> GetTables([FromQuery] TableCollectionQueryParams queryParams)
-    {
-        var (tables, count) = await _tableService.GetAllAsync(queryParams);
-        return Ok(new PaginationResponseDto<IEnumerable<TableDetailDto>>(tables, count, queryParams.Page, queryParams.Size));
-    }
-    
-    [HttpGet]
-    [Route("{tableId:int}")]
-    public async Task<IActionResult> GetTable([FromRoute] int tableId)
-    {
-        var table = await _tableService.GetByIdAsync(tableId);
-        return Ok(new ResponseDto<TableDto>(table));
-    }
-    
-    [HttpPost]
-    public async Task<IActionResult> AddTable([FromBody] CreateTableRequestDto request)
-    {
-        var table = await _tableService.AddAsync(request);
-        return Ok(new ResponseDto<TableDto>(table));
-    }
-    
-    [HttpPut]
-    [Route("{tableId:int}")]
-    public async Task<IActionResult> UpdateTable([FromRoute] int tableId, [FromBody] UpdateTableRequestDto tableDto)
-    {
-        var updatedTable = await _tableService.UpdateAsync(tableId, tableDto);
-        return Ok(new ResponseDto<TableDto>(updatedTable));
-    }
-    
-    [HttpDelete]
-    [Route("{tableId:int}")]
-    public async Task<IActionResult> DeleteTable([FromRoute] int tableId)
-    {
-        await _tableService.DeleteAsync(tableId);
-        return Ok(new ResponseDto<string>("Table deleted successfully"));
-    }
+	private readonly ITableService _tableService;
+
+	public AdminTableController(ITableService tableService)
+	{
+		_tableService = tableService;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetTables([FromQuery] TableCollectionQueryParams queryParams)
+	{
+		var (tables, count) = await _tableService.GetAllAsync(queryParams);
+		return Ok(new PaginationResponseDto<IEnumerable<TableDetailDto>>(tables, count, queryParams.Page, queryParams.Size));
+	}
+
+	[HttpGet]
+	[Route("{tableId:int}")]
+	public async Task<IActionResult> GetTable([FromRoute] int tableId)
+	{
+		var table = await _tableService.GetByIdAsync(tableId);
+		return Ok(new ResponseDto<TableDto>(table));
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> AddTable([FromBody] CreateTableRequestDto request)
+	{
+		if (request.SeatsCount <= 0)
+		{
+			throw new ArgumentException("Seats count must be greater than 0");
+		}
+
+		var table = await _tableService.AddAsync(request);
+		return Ok(new ResponseDto<TableDto>(table));
+	}
+
+	[HttpPut]
+	[Route("{tableId:int}")]
+	public async Task<IActionResult> UpdateTable([FromRoute] int tableId, [FromBody] UpdateTableRequestDto tableDto)
+	{
+		if (tableDto.SeatsCount < 0)
+		{
+			throw new ArgumentException("Number of seats must be greater than 0");
+		}
+
+		var updatedTable = await _tableService.UpdateAsync(tableId, tableDto);
+		return Ok(new ResponseDto<TableDto>(updatedTable));
+	}
+
+	[HttpDelete]
+	[Route("{tableId:int}")]
+	public async Task<IActionResult> DeleteTable([FromRoute] int tableId)
+	{
+		await _tableService.DeleteAsync(tableId);
+		return Ok(new ResponseDto<string>("Table deleted successfully"));
+	}
 }
---
+++ BistroQ.Tests/AdminTableControllerTest.cs
@@ -0,0 +1,209 @@
+﻿using BistroQ.API.Controllers;
+using BistroQ.Core.Dtos;
+using BistroQ.Core.Dtos.Tables;
+using BistroQ.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace BistroQ.Tests.Controllers
+{
+	[TestClass]
+	public class AdminTableControllerTests
+	{
+		private Mock<ITableService> _mockTableService;
+		private AdminTableController _adminTableController;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_mockTableService = new Mock<ITableService>();
+			_adminTableController = new AdminTableController(_mockTableService.Object);
+		}
+
+		[TestMethod]
+		public async Task GetTables_Success()
+		{
+			// Arrange
+			var tables = new List<TableDetailDto> { new TableDetailDto { TableId = 1, SeatsCount = 4 } };
+			var queryParams = new TableCollectionQueryParams { Page = 1, Size = 10 };
+			_mockTableService.Setup(x => x.GetAllAsync(queryParams)).ReturnsAsync((tables, tables.Count));
+
+			// Act
+			var result = await _adminTableController.GetTables(queryParams) as OkObjectResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			var response = result.Value as PaginationResponseDto<IEnumerable<TableDetailDto>>;
+			Assert.IsNotNull(response);
+			Assert.AreEqual(true, response.Success);
+			var data = response.Data as IEnumerable<TableDetailDto>;
+			Assert.AreEqual(1, data.Count());
+			Assert.AreEqual(1, response.Data.First().TableId);
+		}
+
+		[TestMethod]
+		public async Task GetTables_ThrowsException_WhenRetrievalFails()
+		{
+			// Arrange
+			var queryParams = new TableCollectionQueryParams { Page = 1, Size = 10 };
+			_mockTableService.Setup(x => x.GetAllAsync(queryParams)).ThrowsAsync(new Exception("Failed to get tables"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<Exception>(async () =>
+			{
+				await _adminTableController.GetTables(queryParams);
+			});
+		}
+
+		[TestMethod]
+		public async Task GetTable_Success()
+		{
+			// Arrange
+			var table = new TableDto { TableId = 1, SeatsCount = 4 };
+			_mockTableService.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(table);
+
+			// Act
+			var result = await _adminTableController.GetTable(1) as OkObjectResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			var response = result.Value as ResponseDto<TableDto>;
+			Assert.IsNotNull(response);
+			Assert.AreEqual(1, response.Data.TableId);
+		}
+
+		[TestMethod]
+		public async Task GetTable_ThrowsException_WhenRetrievalFails()
+		{
+			// Arrange
+			_mockTableService.Setup(x => x.GetByIdAsync(1)).ThrowsAsync(new Exception("Failed to get table"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<Exception>(async () =>
+			{
+				await _adminTableController.GetTable(1);
+			});
+		}
+
+		[TestMethod]
+		public async Task AddTable_Success()
+		{
+			// Arrange
+			var request = new CreateTableRequestDto { ZoneId = 1, SeatsCount = 4 };
+			var table = new TableDto { TableId = 1, SeatsCount = 4 };
+			_mockTableService.Setup(x => x.AddAsync(request)).ReturnsAsync(table);
+
+			// Act
+			var result = await _adminTableController.AddTable(request) as OkObjectResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			var response = result.Value as ResponseDto<TableDto>;
+			Assert.IsNotNull(response);
+			Assert.AreEqual(1, response.Data.TableId);
+		}
+
+		[TestMethod]
+		public async Task AddTable_ThrowsException_WhenCreationFails()
+		{
+			// Arrange
+			var request = new CreateTableRequestDto { ZoneId = 1, SeatsCount = 4 };
+			_mockTableService.Setup(x => x.AddAsync(request)).ThrowsAsync(new Exception("Failed to create table"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<Exception>(async () =>
+			{
+				await _adminTableController.AddTable(request);
+			});
+		}
+
+		[TestMethod]
+		public async Task AddTable_ThrowsException_WhenNegativeSeatsCount()
+		{
+			// Arrange
+			var request = new CreateTableRequestDto { ZoneId = 1, SeatsCount = -1 };
+			_mockTableService.Setup(x => x.AddAsync(request)).ThrowsAsync(new ArgumentException("Invalid argument"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
+			{
+				await _adminTableController.AddTable(request);
+			});
+		}
+
+		[TestMethod]
+		public async Task UpdateTable_Success()
+		{
+			// Arrange
+			var request = new UpdateTableRequestDto { ZoneId = 1, SeatsCount = 4 };
+			var table = new TableDto { TableId = 1, SeatsCount = 4 };
+			_mockTableService.Setup(x => x.UpdateAsync(1, request)).ReturnsAsync(table);
+
+			// Act
+			var result = await _adminTableController.UpdateTable(1, request) as OkObjectResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			var response = result.Value as ResponseDto<TableDto>;
+			Assert.IsNotNull(response);
+			Assert.AreEqual(1, response.Data.TableId);
+		}
+
+		[TestMethod]
+		public async Task UpdateTable_ThrowsException_WhenUpdateFails()
+		{
+			// Arrange
+			var request = new UpdateTableRequestDto { ZoneId = 1, SeatsCount = 4 };
+			_mockTableService.Setup(x => x.UpdateAsync(1, request)).ThrowsAsync(new Exception("Failed to update table"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<Exception>(async () =>
+			{
+				await _adminTableController.UpdateTable(1, request);
+			});
+		}
+
+		[TestMethod]
+		public async Task UpdateTable_ThrowsException_WhenNegativeSeatsCount()
+		{
+			// Arrange
+			var request = new UpdateTableRequestDto { ZoneId = 1, SeatsCount = -1 };
+			_mockTableService.Setup(x => x.UpdateAsync(1, request)).ThrowsAsync(new ArgumentException("Invalid argument"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
+			{
+				await _adminTableController.UpdateTable(1, request);
+			});
+		}
+
+		[TestMethod]
+		public async Task DeleteTable_Success()
+		{
+			// Arrange
+			_mockTableService.Setup(x => x.DeleteAsync(1)).Returns(Task.CompletedTask);
+
+			// Act
+			var result = await _adminTableController.DeleteTable(1) as OkObjectResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			var response = result.Value as ResponseDto<string>;
+			Assert.IsNotNull(response);
+			Assert.AreEqual("Table deleted successfully", response.Data);
+		}
+
+		[TestMethod]
+		public async Task DeleteTable_ThrowsException_WhenDeletionFails()
+		{
+			// Arrange
+			_mockTableService.Setup(x => x.DeleteAsync(1)).ThrowsAsync(new Exception("Failed to delete table"));
+
+			// Act & Assert
+			await Assert.ThrowsExceptionAsync<Exception>(async () =>
+			{
+				await _adminTableController.DeleteTable(1);
+			});
+		}
+	}
+}
